872e41269071ddddc62aba749bae891f739fa97e
Brett Chapin <brett.chapin89@gmail.com>
2024-06-17 23:48:32 -0400

    fixup! Updated project to use custom SwiftLint config

--- Columbia City Christian ChurchTests/AppTests.swift
+++ Columbia City Christian ChurchTests/AppTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Columbia_City_Christian_Church
 
-final class Columbia_City_Christian_ChurchTests: XCTestCase {
+final class AppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- Columbia City Christian ChurchUITests/AppUITests.swift
+++ Columbia City Christian ChurchUITests/AppUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class Columbia_City_Christian_ChurchUITests: XCTestCase {
+final class AppUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
