f0b0f7a5e00dbf35e750d9be857a78a7d25ce9a8
Brett Chapin <brett.chapin89@gmail.com>
2024-06-18 00:51:23 -0400

    Added the skeleton for the DashboardViewModel

---
+++ Columbia City Christian Church/ViewModels/DashboardViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  DashboardViewModel.swift
+//  Columbia City Christian Church
+//
+//  Created by Brett Chapin on 6/18/24.
+//
+
+import SwiftUI
+
+extension Dashboard {
+    @Observable
+    class ViewModel {
+        
+    }
+}
