51aa65bb171280d9f18aec81df3c7d274349094a
Brett Chapin <brett.chapin89@gmail.com>
2024-06-17 22:05:49 -0400

    Create Xcode Project
    Updated some basic project settings like OS support, orientation support, and app category.

---
+++ Columbia City Christian Church/Columbia_City_Christian_ChurchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Columbia_City_Christian_ChurchApp.swift
+//  Columbia City Christian Church
+//
+//  Created by Brett Chapin on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Columbia_City_Christian_ChurchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
