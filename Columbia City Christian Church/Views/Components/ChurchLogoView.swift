3f00ab8b55012d99e59e192e483b246010762c74
Brett Chapin <brett.chapin89@gmail.com>
2024-06-18 00:48:02 -0400

    Added the components for the ChurchButton
    Which include the ChurchLogoView and ChurchLogoShape, along with the Primary, Secondary, and Tertiary church color assets.

---
+++ Columbia City Christian Church/Views/Components/ChurchLogoView.swift
@@ -0,0 +1,28 @@
+//
+//  ChurchLogoView.swift
+//  Columbia City Christian Church
+//
+//  Created by Brett Chapin on 6/18/24.
+//
+
+import SwiftUI
+
+struct ChurchLogoView: View {
+    var body: some View {
+        GeometryReader { proxy in
+            let measurement = proxy.size.width > proxy.size.height ? proxy.size.height : proxy.size.width
+            let radius = measurement / 2
+            let startRadius = radius * 0.15
+            
+            ChurchIconShape()
+                .fill(RadialGradient(colors: [.churchSecondary, .churchTertiary, .churchPrimary], center: .top, startRadius: startRadius, endRadius: measurement))
+                .fill(LinearGradient(colors: [.clear, .clear, .black.opacity(0.3)], startPoint: .top, endPoint: .bottom))
+        }
+        .aspectRatio(contentMode: .fit)
+    }
+}
+
+#Preview {
+    ChurchLogoView()
+        .frame(width: 200, height: 200)
+}
