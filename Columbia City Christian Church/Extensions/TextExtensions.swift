9be296d84a71b8d8eab2ef728e9df48501c7976e
Brett Chapin <brett.chapin89@gmail.com>
2024-06-19 14:29:06 -0400

    Removed fontSize from TextModifier

--- Columbia City Christian Church/Extensions/TextExtensions.swift
+++ Columbia City Christian Church/Extensions/TextExtensions.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 extension Text {
-    func themeStyle(_ style: TextModifier.FontStyle, 
+    /**
+     A custom modifier for Text views that applies the `TextModifier` ViewModifier. Which allows for quick and uniform formattering.
+     
+     - Parameter style: This is the font style for the Text view, this controls font characteristic of the Text view.
+     - Parameter lineSpacing: This CGFloat controlls the spacing in points between multiline text. By default this is 1.
+     - Parameter fontColor: This represents the color for the Text, by default this is `.textPrimary`.
+     */
+    func themeStyle(_ style: TextModifier.FontStyle,
                     lineSpacing: CGFloat = 1,
-                    fontColor: Color = .textPrimary,
-                    fontSize: CGFloat? = nil) -> some View {
-        self.modifier(TextModifier(style: style, lineSpacing: lineSpacing, fontColor: fontColor, fontSize: fontSize))
+                    fontColor: Color = .textPrimary) -> some View {
+        self.modifier(TextModifier(style: style, lineSpacing: lineSpacing, fontColor: fontColor))
     }
 }
