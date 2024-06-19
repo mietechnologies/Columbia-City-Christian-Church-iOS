3cd51aa5e92e2e490db7b49b2494b605e1873fe8
Brett Chapin <brett.chapin89@gmail.com>
2024-06-19 13:16:53 -0400

    Updated Dashboard to work with the new ScrollingTabView

--- Columbia City Christian Church/Views/Dashboard.swift
+++ Columbia City Christian Church/Views/Dashboard.swift
@@ -8,9 +8,51 @@
 import SwiftUI
 
 struct Dashboard: View {
+    
+    @State private var tabOptions: [ScrollingTabModel] = [
+        .init(id: .dashboard),
+        .init(id: .checkIn),
+        .init(id: .tithe),
+        .init(id: .sermonNotes),
+        .init(id: .prayerChain),
+        .init(id: .events)
+    ]
+    @State private var activeTab: ScrollingTabModel.Tab = .dashboard
+    @State private var mainViewScrollState: ScrollingTabModel.Tab?
+    
     var body: some View {
         NavigationStack {
-            Text("Hello World!")
+            VStack(spacing: 0) {
+                
+                ScrollingTabView(tabOptions: $tabOptions, currentTab: $activeTab)
+                    .padding(.horizontal, 5)
+                    .onChange(of: activeTab) { _, newValue in
+                        mainViewScrollState = newValue
+                    }
+                
+                GeometryReader { proxy in
+                    let size = proxy.size
+                    
+                    ScrollView(.horizontal) {
+                        LazyHStack(spacing: 0) {
+                            ForEach(tabOptions) { tab in
+                                tab.screen()
+                                    .frame(width: size.width, height: size.height)
+                                    .contentShape(Rectangle())
+                            }
+                        }
+                        .scrollTargetLayout()
+                    }
+                    .scrollPosition(id: $mainViewScrollState)
+                    .scrollIndicators(.hidden)
+                    .scrollTargetBehavior(.paging)
+                    .background(.white)
+                    .onChange(of: mainViewScrollState) { _, newValue in
+                        if let newValue {
+                            activeTab = newValue
+                        }
+                    }
+                }
                 .navigationTitle("Columbia City Christian Church")
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar {
@@ -26,7 +68,15 @@ struct Dashboard: View {
                         }
                     }
                 }
+            }
+            .background(.navigationBarBackground)
         }
+    }
+}
+
+struct TestView: View {
+    var body: some View {
+        Text("Hello World!")
     }
 }
 
