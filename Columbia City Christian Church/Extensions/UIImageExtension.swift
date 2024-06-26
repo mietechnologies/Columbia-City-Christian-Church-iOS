020460b8655f94f5697d639eae063db4dfaeb4a0
Brett Chapin <brett.chapin89@gmail.com>
2024-06-25 22:33:17 -0400

    Added DocStrings

--- Columbia City Christian Church/Extensions/UIImageExtension.swift
+++ Columbia City Christian Church/Extensions/UIImageExtension.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 extension UIImage {
+    /**
+     A method to crop the given image, `self`, to a specific value within its frame.
+     
+     - Parameter rect: A `CGRect` that represents the location and size of the crop relative to the current image.
+     - Returns: The cropped `UIImage` from the starting image.
+     */
     func cropTo(_ rect: CGRect) -> UIImage? {
         guard let cgImage = self.cgImage else { return nil }
         guard let croppedCGImage = cgImage.cropping(to: rect) else { return nil }
