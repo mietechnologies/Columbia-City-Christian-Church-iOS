be8c103093e7bae2ff43d51916b5b1cb885ba571
Brett Chapin <brett.chapin89@gmail.com>
2025-12-28 23:13:49 -0500

    Added initial implementation of App and ContentView

---
+++ Columbia City Christian Church/ColumbiaCityChristianChurchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Columbia_City_Christian_ChurchApp.swift
+//  Columbia City Christian Church
+//
+//  Created by Brett Chapin on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ColumbiaCityChristianChurchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Columbia City Christian Church/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Columbia City Christian Church
+//
+//  Created by Brett Chapin on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
