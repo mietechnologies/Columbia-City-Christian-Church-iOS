bfffe5cd52606889dedcb6ab1135ffad2886d36e
Brett Chapin <brett.chapin89@gmail.com>
2025-12-28 23:16:32 -0500

    Added OptionalExtension

---
+++ Columbia City Christian Church/Extensions/OptionalExtension.swift
@@ -0,0 +1,12 @@
+//
+//  OptionalExtension.swift
+//  Columbia City Christian Church
+//
+//  Created by Brett Chapin on 2025.12.28.
+//
+
+extension Optional {
+    var isNil: Bool {
+        self == nil
+    }
+}
